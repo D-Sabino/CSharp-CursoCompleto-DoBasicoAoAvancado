fbb3ca85402c4eae8ae18d98c2b0b29cfca2101b
Daniel Sabino <dannisabino17@gmail.com>
2023-05-08 07:37:09 -0300

    Aula 17: Primeiro Programa

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
