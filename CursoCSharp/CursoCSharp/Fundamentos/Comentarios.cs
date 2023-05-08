aefc7a4b8a915e5eec7cff587f6053b7d90be363
Daniel Sabino <dannisabino17@gmail.com>
2023-05-08 07:54:13 -0300

    Aula 20: Comentarios de Codigo

---
+++ CursoCSharp/CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Comentarios
+    {
+        public static void Executar()
+        {
+            // Cuidado com os comentarios desnecessarios
+            Console.WriteLine("Codigo claro é sempre melhor!");
+
+            /*
+             * Esse é um comentario
+             * de multiplas linhas...
+             */
+            Console.WriteLine("O C# tem o XML Comments");
+
+            //Console.WriteLine("1");
+            //Console.WriteLine("2");
+        }
+    }
+}
