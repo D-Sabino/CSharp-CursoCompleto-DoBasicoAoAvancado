433dd551c1b814499a1cecc4329007b1a2d33cc0
Daniel Sabino <dannisabino17@gmail.com>
2023-05-19 18:34:06 -0300

    Aula 26: Formatando Números

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -14,6 +14,7 @@ namespace CursoCSharp {
                 {"Interpolacao - Fundamentos", Interpolacao.Executar},
                 {"Notacao Ponto - Fundamentos", NotacaoPonto.Executar},
                 {"Lendo dados - Fundamentos", LendoDados.Executar},
+                {"Formatando Numero - Fundamentos", FormatandoNumero.Executar},
             });
 
             central.SelecionarEExecutar();
